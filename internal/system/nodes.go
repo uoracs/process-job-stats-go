58cf078a34f8216ac3ffb2df829b0298e82f238d
Lucas Crownover <lcrownover127@gmail.com>
2025-02-12 11:26:06 -0800

    working

--- internal/system/nodes.go
+++ internal/system/nodes.go
@@ -14,6 +14,9 @@ import (
 func expandNodeList(ctx context.Context, nodeList string) (string, error) {
 	slog.Debug("  Started: Expanding nodelist")
 	slurmBinDir := ctx.Value(types.SlurmBinDirKey)
+	if strings.TrimSpace(nodeList) == "" {
+		return "", nil
+	}
 	if slurmBinDir == nil {
 		return "", fmt.Errorf("failed to find slurm bin dir in context")
 	}
