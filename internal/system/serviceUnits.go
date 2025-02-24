73140dc081d3d3e98aaa501c8fc8e03dee3b3294
Lucas Crownover <lcrownover127@gmail.com>
2025-02-24 14:37:29 -0800

    better calc for service units

--- internal/system/serviceUnits.go
+++ internal/system/serviceUnits.go
@@ -1,6 +1,18 @@
 package system
 
-func calculateServiceUnits(cpuHoursOpenUse, cpuHoursCondo, gpuHoursOpenUse, gpuHoursCondo float64) float64 {
-	// so far it's just 1 * cpu hours and 3* gpu hours
-	return ((cpuHoursOpenUse + cpuHoursCondo) * 1) + ((gpuHoursOpenUse + gpuHoursCondo) * 3)
+import "github.com/lcrownover/process-job-stats-go/internal/types"
+
+var preemptWeight = 1
+
+func calculateServiceUnits(category types.JobCategory, cpuHoursOpenUse, cpuHoursCondo, gpuHoursOpenUse, gpuHoursCondo float64) float64 {
+	// open use -> just return open use cpus + 3x gpus
+	if category == types.JobCategoryOpen {
+		return cpuHoursOpenUse + (gpuHoursOpenUse * 3)
+	}
+	// condo -> return 0
+	if category == types.JobCategoryCondo {
+		return 0
+	}
+	// otherwise its preempt
+	return (((cpuHoursOpenUse + cpuHoursCondo) * 1) + ((gpuHoursOpenUse + gpuHoursCondo) * 3)) * float64(preemptWeight)
 }
